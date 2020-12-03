ecd3bb087f54dd9f130e5bb2f13bc0798a57e158
Christian Halaszovich <chalaszovich@gmail.com>
2020-12-03 21:43:38 +0100

    auto-label leak trace only if no trace label was provided in dat file

--- pmbrowserwindow.cpp
+++ pmbrowserwindow.cpp
@@ -82,12 +82,16 @@ void PMbrowserWindow::populateTreeView()
                         tracelabel = "Imon";
                     } else if (datakind & IsVmon) {
                         tracelabel = "Vmon";
-                    }else if(datakind & IsLeak){
-                        tracelabel = "Leak";
                     } else {
                         tracelabel = trace.getString(TrLabel).c_str();
                         if (tracelabel.isEmpty()) {
-                            tracelabel = QString("trace %1").arg(l); }
+                            if (datakind & IsLeak) {
+                                tracelabel = "Leak";
+                            }
+                            else {
+                                tracelabel = QString("trace %1").arg(l);
+                            }
+                        }
                     }
                     auto traceitem = new QTreeWidgetItem(sweepitem,QStringList(tracelabel));
                     traceitem->setData(0,Qt::UserRole, QVariant::fromValue(&trace)); // store pointer to trace for later use
