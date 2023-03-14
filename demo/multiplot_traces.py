f08fd693edc297e068c237de283d9ea173790e6d
Christian Halaszovich <chalaszovich@gmail.com>
2023-03-14 16:05:36 +0100

    some python scripts demostating platting of multiple traces

---
+++ demo/multiplot_traces.py
@@ -0,0 +1,37 @@
+import os.path
+import sys
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+def getMeta(npy_filename):
+    json_filename=os.path.splitext(npy_filename)[0]+".json"
+    with open(json_filename, "r", encoding='latin1') as f:
+        meta=json.load(f)
+    return meta
+
+def createX(meta):
+    x0=meta['x_0']
+    deltax=meta['delta_x']
+    numpnts=meta['numpnts']
+    lastx=x0+deltax*numpnts
+    x=np.linspace(x0,lastx,numpnts)
+    return x
+
+if __name__ == '__main__':
+    if(len(sys.argv)<2):
+        print("usage: multiplot_traces <filename1>.npy [<file2>.npy ...]",file=sys.stderr)
+        sys.exit(-1)
+    fig, ax = plt.subplots()
+    for index in range(1,len(sys.argv)): 
+        npy_filename=sys.argv[index]
+        basename=os.path.basename(os.path.splitext(npy_filename)[0])
+        meta=getMeta(npy_filename)
+        x_w=createX(meta)
+        y_w=np.load(npy_filename)
+        ax.plot(x_w,y_w,label=basename)
+    ax.legend()
+    ax.set_xlabel('['+meta['unit_x']+']')
+    ax.set_ylabel('['+meta['unit_y']+']')
+    plt.show()
+
