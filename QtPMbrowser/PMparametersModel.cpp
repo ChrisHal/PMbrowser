946477bfd978d9d16abe541c0f508e8b83f32923
Christian Halaszovich <chalaszovich@gmail.com>
2025-08-20 16:28:58 +0200

    introduce a model to modify PMparameters

---
+++ QtPMbrowser/PMparametersModel.cpp
@@ -0,0 +1,72 @@
+#include <QString>
+#include "PMparametersModel.h"
+
+PMparametersModel::PMparametersModel(QSpan<hkLib::PMparameter> p, QObject* parent)
+	: QAbstractTableModel(parent), parameters(p)
+{}
+
+PMparametersModel::~PMparametersModel()
+{}
+
+int PMparametersModel::rowCount(const QModelIndex & parent) const
+{
+	return parameters.size();
+}
+
+int PMparametersModel::columnCount(const QModelIndex& parent) const
+{
+	return 2;
+}
+
+QVariant PMparametersModel::data(const QModelIndex& index, int role) const
+{
+    int row = index.row();
+    int col = index.column();
+    constexpr std::array<const char*, 2> lables{"export", "print"};
+    const auto& p = parameters[row];
+    switch (role) {
+    case Qt::CheckStateRole:
+        if (col == 0) {
+            return p.exportIBW ? Qt::Checked : Qt::Unchecked;
+        }
+        else if (col == 1) {
+            return p.print ? Qt::Checked : Qt::Unchecked;
+        }
+        break;
+    case Qt::DisplayRole:
+        return QString::fromUtf8(lables.at(col));
+        break;
+    }
+    return QVariant();
+}
+
+QVariant PMparametersModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role == Qt::DisplayRole && orientation == Qt::Vertical) {
+        return QString::fromUtf8(parameters[section].name);
+    }
+    return QVariant();
+}
+
+bool PMparametersModel::setData(const QModelIndex& index, const QVariant& value, int role)
+{
+    if (role == Qt::CheckStateRole) {
+        if (!checkIndex(index))
+            return false;
+        auto& p = parameters[index.row()];
+        auto state = value.value<Qt::CheckState>();
+        if (index.column() == 0) {
+            p.exportIBW = state != Qt::Unchecked;
+        } else if (index.column() == 1) {
+            p.print = state != Qt::Unchecked;
+        }
+        return true;
+    }
+    return false;
+}
+
+Qt::ItemFlags PMparametersModel::flags(const QModelIndex& index) const
+{
+    return Qt::ItemIsUserCheckable | QAbstractTableModel::flags(index);
+}
+
---
+++ QtPMbrowser/PMparametersModel.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QAbstractTableModel>
+#include <QSpan>
+#include "PMparameters.h"
+
+class PMparametersModel  : public QAbstractTableModel
+{
+	Q_OBJECT
+
+private:
+	QSpan<hkLib::PMparameter> parameters;
+
+public:
+	explicit PMparametersModel(QSpan<hkLib::PMparameter> PMparameters, QObject *parent = nullptr);
+	~PMparametersModel();
+	int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+	int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+	QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+	QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+	bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole) override;
+	Qt::ItemFlags flags(const QModelIndex& index) const override;
+};
+
